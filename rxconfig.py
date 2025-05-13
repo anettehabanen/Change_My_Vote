e9fa5fcf3dd04fee30b14597dd2276df58a91fd5
ahabanen <anette.habanen@cyber.ee>
2025-05-13 11:38:01 +0300

    changed log level

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="Reflex_Change_My_Vote",
     deploy_url="http://localhost:3001",
-    loglevel = "debug",
+    loglevel = "default",
     frontend_port=3001,
     backend_port=8001,
 )
